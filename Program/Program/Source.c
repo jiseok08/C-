ab936bc466057b49a5af6f87e947162b13f3a4ca
jiseok08 <heoji0912@gmail.com>
2024-11-28 19:46:40 +0900

    (2024.11.28) 재귀 함수

--- Program/Program/Source.c
+++ Program/Program/Source.c
@@ -27,13 +27,35 @@ int Compare(float x, float y)
 	}
 }
 
-void Swap(int left, int right)
+void Swap(int * left, int * right)
 {
-	int temporary = left;
-	left = right;
-	right = temporary;
+	int temporary = *left;
+	*left = *right;
+	*right = temporary;
 }
 
+inline void Process(float progress)
+{
+	printf("%f\n", progress);
+
+	// 인라인 함수는 컴파일 시점에 확장되며, 컴파일시
+	// 인라인 함수로 선언하더라도 상황에 따라
+	// 일반 함수로 변환되기도 합니다
+}
+
+void Recursive(int count)
+{
+	if (count > 0)
+	{
+		Recursive(count - 1);
+	}
+	else
+	{
+		return;
+	}
+
+	printf("Recursive Fucntion\n");
+}
 
 void main()
 
@@ -72,12 +94,12 @@ void main()
 	// 함수가 호출될 때 매개 변수에 실제로 전달되는
 	// 값입니다.
 
-	int a = 10;
-	int b = 20;
-	Swap(a, b);
-	
-	printf("a의 값 : %d\n", a);
-	printf("b의 값 : %d\n", b);
+	//int a = 10;
+	//int b = 20;
+	//Swap(&a, &b);
+	//
+	//printf("a의 값 : %d\n", a);
+	//printf("b의 값 : %d\n", b);
 
 
 	// 인수의 경우 함수에 있는 매개 변수의 수에 따라
@@ -87,6 +109,29 @@ void main()
 
 #pragma endregion
 
+#pragma region 인라인 함수
+	// 함수를 호출하는 대신 함수가 호출되는 위치마다
+	// 함수의 코드를 복사하여 전달하는 방식의 함수입니다.
+
+	// Process(46.7f);
+
+	// 인라인 함수는 함수를 호출하는 과정이 없으므로 처리 속도가
+	// 빠르지만, 인라인 함수를 많이 사용하게 되면 함수의 코드가 
+	// 복사되기 때문에 실행 파일의 크기가 커지게 됩니다.
+#pragma endregion
+
+#pragma region 재귀함수
+	// 어떤 함수에서 자신을 다시 호출하여 작업을
+	// 수행하는 함수입니다.
+
+	// Recursive(3);
+
+	// 재귀 함수는 함수를 게속 호출하기 때문에 스택 영역에
+	// 메모리가 계속 쌓이게 되므로 스택 오버플로우가
+	// 발생하게 됩니다.
+
+
+#pragma endregion
 
 
 }
